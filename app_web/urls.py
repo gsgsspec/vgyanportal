e6bf57ab3cecbde8fc61b443810ac8f4315ee888
Pratap0101 <pratap.medisetty@gsspec.com>
2023-11-13 19:15:14 +0530

    urls_my_courses

--- app_web/urls.py
+++ app_web/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.loginPage)
+    path('',views.loginPage),
+    path('my-courses',views.myCourses)
 ]
--- app_web/views.py
+++ app_web/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 def loginPage(request):
     try:
 
-
         return render(request,'login.html')
+    
+    except Exception as e:
+        raise
+
+
+def myCourses(request):
+    try:
+
+        return render(request,'index.html',{'template_name':'myCourses.html'})
     except Exception as e:
         raise
