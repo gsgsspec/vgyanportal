34f95686d9d8c4fa42bfa48d69148d93f5667022
Srikanth G <srikanth@gsspec.com>
2023-11-20 12:23:57 +0530

    course details js

---
+++ app_web/static/js/course_details.js
@@ -0,0 +1,92 @@
+document.getElementById('save_rating').onclick=function(){
+
+    $('#rating_form').unbind('submit').bind('submit',function(event){
+        event.preventDefault();
+       
+        dataObj = {
+            'rating':  $('input[name=rating_star]:checked').val(),
+            'comments': $('#rating_comment').val(),
+            'course_id': $('#courseid').attr('name'),
+        }
+
+        var final_data = {
+            'data': JSON.stringify(dataObj),
+            csrfmiddlewaretoken: CSRF_TOKEN,
+        }
+
+        $.post(CONFIG['domain'] + "/api/save-rating", final_data, function (res) {
+            console.log('res',res);
+        
+            if (res.statusCode == 0){
+                if (res.token == 'token_generated'){
+                    window.location.href = '/courses';
+                }
+            }
+        })
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).on({
+    mouseover: function(event) {
+        $(this).find('.far').addClass('star-over');
+        $(this).prevAll().find('.far').addClass('star-over');
+    },
+    mouseleave: function(event) {
+        $(this).find('.far').removeClass('star-over');
+        $(this).prevAll().find('.far').removeClass('star-over');
+    }
+}, '.rate');
+
+
+$(document).on('click', '.rate', function() {
+    if ( !$(this).find('.star').hasClass('rate-active') ) {
+        $(this).siblings().find('.star').addClass('far').removeClass('fas rate-active');
+        $(this).find('.star').addClass('rate-active fas').removeClass('far star-over');
+        $(this).prevAll().find('.star').addClass('fas').removeClass('far star-over');
+    } else {
+        console.log('has');
+    }
+});
+    
