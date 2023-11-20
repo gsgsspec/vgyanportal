da436147f3878c516067354ce57d08642ba56dd5
Pratap0101 <pratap.medisetty@gsspec.com>
2023-11-20 10:59:19 +0530

    ask-a-question

---
+++ app_web/static/js/ask_Aquestion.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//     askQuestionInputField()
+// })
+
+
+// function askQuestionInputField(){
+//     $('#askaQuestionCardContainer').append(
+
+//         ' <div class="col-12 col-md-12 col-lg-6">' +
+//                         '<div class="card mb-3">'  +
+//                           '<div class="my-course-card">' +
+//                             '<div class="m-3 col-md-4 my-courses-images"></div>' +
+//                             '<div class="col-md-8 my-course-card-body">' +
+//                               '<div class="card-body m-0 p-0">' +
+//                                 '<h5 class="card-title"></h5>'  +
+//                                 '<p> </p>' +
+                                
+//                               '</div>' +
+//                             '</div>'   +
+//                          '</div>'      +
+//                         '</div>'       +
+//                       '</div>'         
+// )}
+
