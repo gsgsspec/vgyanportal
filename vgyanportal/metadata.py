ef04829039c8baa36998f1670b57cd24b55a58d2
Srikanth G <srikanth@gsspec.com>
2024-01-02 15:29:55 +0530

    referrer checks

--- vgyanportal/metadata.py
+++ vgyanportal/metadata.py
@@ -55,7 +55,7 @@ def check_referrer(request):
         referrer = request.META['HTTP_REFERER']
         referrer_host = urlsplit(referrer).hostname
 
-        allowed_referrers = ['134.122.25.72:8101']
+        allowed_referrers = ['134.122.25.72']
 
         if referrer_host not in allowed_referrers:
             return False
